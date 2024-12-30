5ab3260856ec893c0be5bfa5f16dee3677392cb8
malshqar <m12342al@gmail.com>
2024-12-30 15:14:49 +0200

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mvc/app',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'elframework/framework' => array(
+            'pretty_version' => '1.0',
+            'version' => '1.0.0.0',
+            'reference' => 'fb65be7b3f2a239e368f5b3c62d485540b86f537',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../elframework/framework',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mvc/app' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
